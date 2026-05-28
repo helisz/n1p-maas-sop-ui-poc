38e71696c4f1ca4f7de8a03aab9d6c649a58c00e
CITIC1616\paripeng <paripeng@citictel.com>
2026-05-28 16:16:26 +0800

    fix(api): correct endpoint URL for account statistics fetching

--- src/apis/acc/$getAccountStatistics.ts
+++ src/apis/acc/$getAccountStatistics.ts
@@ -1,7 +1,7 @@
 // [AI_START TIMESTAMP=2025-07-21 11:30:00]
 
 export function $getAccountStatistics() {
-  return apiFetch<AccountStatisticsDTO>('/api/v1/admin/acc/accounts/statistic', {
+  return apiFetch<AccountStatisticsDTO>('/api/v1/admin/acc/accounts/statistics', {
     method: 'GET',
   });
 }
